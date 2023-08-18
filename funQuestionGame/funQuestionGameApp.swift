0639807fb397b10a087e38cb54cff5a35459563d
Priya <priyapatel7989@gmail.com>
2023-08-17 22:16:35 -0500

    Initial Commit

---
+++ funQuestionGame/funQuestionGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  funQuestionGameApp.swift
+//  funQuestionGame
+//
+//  Created by scholar on 8/17/23.
+//
+
+import SwiftUI
+
+@main
+struct funQuestionGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
